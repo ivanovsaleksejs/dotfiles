2097c14e02086ee06116b0a3414acfa74225762e
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2018-01-18 05:23:00 +0200

    Larger shutdown menu

--- i3/shutdown_menu.sh
+++ i3/shutdown_menu.sh
@@ -3,8 +3,9 @@
 background="#121312"
 foreground="#678096"
 selected="#43AAE5"
+selectedb="#678096"
 
-select=$(echo -e 'Suspend\nReboot\nShutdown' | dmenu -nb ${background} -nf ${foreground} -sb ${background} -sf ${selected} -fn 'Source Code Pro-16' -m 0 -i -p "What would you like to do?")
+select=$(echo -e 'Suspend\nReboot\nShutdown' | dmenu -nb ${background} -nf ${foreground} -sb ${background} -sf ${selected} -fn 'Source Code Pro-16' -m 0 -l 3 -i -p "What would you like to do?")
 
 case ${select} in
         "Suspend")
