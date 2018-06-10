e3d067e896a1738968e4f884d2a8bdb37a8d2b2a
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2018-06-10 22:31:44 +0300

    Running apps dmenu

--- i3/install.sh
+++ i3/install.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 ln -s ${PWD}/config ${HOME}/.config/i3/config
-ln -s ${PWD}/*.png ${HOME}/.config/i3/
 ln -s ${PWD}/shutdown_menu.sh ${HOME}/.config/i3/
 ln -s ${PWD}/shortcuts.sh ${HOME}/.config/i3/
 ln -s ${PWD}/dmenu_nixos.sh ${HOME}/.config/i3/
+ln -s ${PWD}/dmenu_running.sh ${HOME}/.config/i3/
 ln -s ${PWD}/lock.sh ${HOME}/.config/i3/
 ln -s ${PWD}/lastfm_wp.php ${HOME}/.config/i3/
 mkdir ${HOME}/.config/i3/images
